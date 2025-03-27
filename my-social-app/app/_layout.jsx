c764f9e8033dd8b14bbd637688e794450825b690
emmanuel <emmanuelsaura008@gmail.com>
2025-03-27 20:14:33 +0300

    supabase auth

--- my-social-app/app/_layout.jsx
+++ my-social-app/app/_layout.jsx
@@ -2,6 +2,7 @@ import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { supabase } from "@/lib/superbase";
 import { Stack, useRouter } from "expo-router";
 import { useEffect } from "react";
+import { getUserData } from "@/services/userService"
 
 export default function RootLayout() {
   return (
@@ -12,16 +13,20 @@ export default function RootLayout() {
 }
 
 const MainLayout = () => {
-  const { setAuth } = useAuth()
+  const { setAuth, setUserData } = useAuth()
   const router = useRouter()
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
-      console.log("user Session: ....", session?.user)
+      // console.log("user Session: ....", session?.user)
 
       if (session) {
 
         //set auth
         setAuth(session?.user)
+
+        updateUserData(session?.user)
+
+
         //move to home screen
         router.replace("/home")
 
@@ -35,6 +40,14 @@ const MainLayout = () => {
 
     })
   }, [])
+
+
+  const updateUserData = async (user) => {
+    let res = await getUserData(user?.id)
+    if (res.success) {
+      setUserData(res.data)
+    }
+  }
 
 
   return <Stack screenOptions={{ headerShown: false }} />;
