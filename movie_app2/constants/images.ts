e79410f1376731dbf0cbcc50e07741e667d1d4ba
emmanuel <emmanuelsaura008@gmail.com>
2025-05-15 08:39:05 +0300

    app 2

--- movie_app2/constants/images.ts
+++ movie_app2/constants/images.ts
@@ -1,10 +1,11 @@
 import bg from "@/assets/images/bg.png";
 import highlight from "@/assets/images/highlight.png";
-// import rankingGradient from "@/assets/images/rankingGradient.png";
+import rankingGradient from "@/assets/images/rankingGradient.png";
 import mainbg from "@/assets/images/watermark.png";
 
 export const images = {
   bg,
   highlight,
-  mainbg
+  mainbg,
+  rankingGradient
 };
